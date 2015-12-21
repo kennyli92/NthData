7588ef9c51bc11e193e02516f76a296b2a459a3d
Huy <huy7070@gmail.com>
2015-12-20 20:08:40 -0600

    Bootstrap to text input

--- BigDataFrontEnd/BigDataFrontEnd/signup/forms.py
+++ BigDataFrontEnd/BigDataFrontEnd/signup/forms.py
@@ -1,14 +1,14 @@
 ﻿from django import forms
 from django.contrib.auth.models import User
-
+from django.utils.translation import ugettext_lazy as _
 #class SignUpForm(forms.ModelForm):
 #    class Meta:
 #        model = User 
 #        fields = ['username', 'password', 'email', 'first_name', 'last_name']
 
 class SignUpForm(forms.Form):
-    username = forms.CharField()
-    password = forms.PasswordInput()
-    email = forms.EmailField()
-    first_name = forms.CharField()
-    last_name = forms.CharField()
+    username = forms.CharField(label='Username', label_suffix='',widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='Password', label_suffix='',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label='Email', label_suffix='',widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(label='First name', label_suffix='',widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(label='Last name', label_suffix='',widget=forms.TextInput(attrs={'class':'form-control'}))
