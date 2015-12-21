aa65ac587b44cffbe804678629f207d9a533c558
Huy <huy7070@gmail.com>
2015-12-21 16:18:32 -0600

    Password Validation checker

--- BigDataFrontEnd/BigDataFrontEnd/signup/forms.py
+++ BigDataFrontEnd/BigDataFrontEnd/signup/forms.py
@@ -9,6 +9,26 @@ from django.utils.translation import ugettext_lazy as _
 class SignUpForm(forms.Form):
     username = forms.CharField(label='Username', label_suffix='',widget=forms.TextInput(attrs={'class':'form-control'}))
     password = forms.CharField(label='Password', label_suffix='',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', label_suffix='',widget=forms.PasswordInput(attrs={'class':'form-control'}))
     email = forms.EmailField(label='Email', label_suffix='',widget=forms.EmailInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(label='First name', label_suffix='',widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(label='Last name', label_suffix='',widget=forms.TextInput(attrs={'class':'form-control'}))
+
+
+    def clean(self):
+        errors = []
+        if 'password' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password'] != self.cleaned_data['password2']:
+                errors.append(_("The two password fields did not match."))
+
+        if 'email' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['email'] != self.cleaned_data['password2']:
+                errors.append(_("Email Test Error!"))
+
+        if errors:
+            raise forms.ValidationError(errors)
+
+        return self.cleaned_data
+
+
+
