9779222b5a40f9595d36f52bc51b12d6e870aa87
Huy <huy7070@gmail.com>
2015-12-21 13:46:17 -0600

    Added sign up controller

--- BigDataFrontEnd/BigDataFrontEnd/signup/views.py
+++ BigDataFrontEnd/BigDataFrontEnd/signup/views.py
@@ -2,7 +2,36 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 
+#def register(request):
+#    form = SignUpForm()
+#    return render(request, 'signup/signup.html', {'form': form})
+
 def register(request):
-    form = SignUpForm()
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SignUpForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+
+            user = User.objects.create_user(
+            username,
+            password,
+            email,
+            first_name,
+            last_name
+            )
+            # redirect to a new URL:
+            return HttpResponseRedirect('signup/profile.html')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SignUpForm()
 
     return render(request, 'signup/signup.html', {'form': form})
